58d24596a95a0ed6bce91d34e746b5f5a8b57a3d
iPakTulane <ipak.tulane@gmail.com>
2023-08-25 15:37:32 +0500

    Add Firebase files

--- ToDoListSwiftUI/ToDoListSwiftUIApp.swift
+++ ToDoListSwiftUI/ToDoListSwiftUIApp.swift
@@ -4,11 +4,16 @@
 //
 //  Created by iPak Tulane on 25/08/23.
 //
-
+import FirebaseCore
 import SwiftUI
 
 @main
 struct ToDoListSwiftUIApp: App {
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
