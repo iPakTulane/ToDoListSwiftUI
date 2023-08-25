355679d752800048dcfa984da55f35959ddedda9
iPakTulane <ipak.tulane@gmail.com>
2023-08-25 14:16:53 +0500

    Initial commit

---
+++ ToDoListSwiftUI/ToDoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftUIApp.swift
+//  ToDoListSwiftUI
+//
+//  Created by iPak Tulane on 25/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
