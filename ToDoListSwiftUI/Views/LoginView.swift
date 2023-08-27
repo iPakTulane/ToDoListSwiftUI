8a35272ed15f4906a39435bd0897ad56ead553b4
iPakTulane <ipak.tulane@gmail.com>
2023-08-27 18:44:57 +0500

    Add login form

--- ToDoListSwiftUI/Views/LoginView.swift
+++ ToDoListSwiftUI/Views/LoginView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
             // Header
             HeaderView()
             
             // Login form
-            
+            Form {
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button {
+                    //Attempt log in
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.blue)
+                        
+                        Text("Log In")
+                            .foregroundColor(Color.white)
+                            .bold()
+                        
+                    }
+                }
+                
+            }
             
             // Create account
             
