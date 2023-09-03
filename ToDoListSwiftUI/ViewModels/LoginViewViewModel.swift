d7dd1a3c7580e6fc7c42434a731521a9ef11abc9
iPakTulane <ipak.tulane@gmail.com>
2023-09-03 19:41:37 +0500

    Add validation function and FirebaseAuth to loginViewViewModel

--- ToDoListSwiftUI/ViewModels/LoginViewViewModel.swift
+++ ToDoListSwiftUI/ViewModels/LoginViewViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by iPak Tulane on 25/08/23.
 //
 
+import FirebaseAuth
 import Foundation
 
 class LoginViewViewModel: ObservableObject {
@@ -15,24 +16,31 @@ class LoginViewViewModel: ObservableObject {
     init() {}
     
     func login() {
+        guard validate() else {
+            return
+        }
+        
+        // Try log in
+        Auth.auth().signIn(withEmail: email, password: password)
+        
+        
+    }
+    
+    private func validate() -> Bool {
         errorMessage = ""
         guard !email.trimmingCharacters(in: .whitespaces).isEmpty, //trimmingCharacters = will trigger user taps a whitespace button
               !password.trimmingCharacters(in: .whitespaces).isEmpty else {
 
             errorMessage = "Please fill in all fields."
-            return // return = stop
+            return false // return = stop
         }
         //Standard writing: email@foo.com
         guard email.contains("@") && email.contains(".") else {
             errorMessage = "Please enter valid email."
-            return
+            return false
         }
+        return true
     }
-    
-    func validate() {
-        
-    }
-    
 }
 
 
