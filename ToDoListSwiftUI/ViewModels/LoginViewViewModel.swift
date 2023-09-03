f6a19163265b62bd1e83857d77df34ef4157a828
iPakTulane <ipak.tulane@gmail.com>
2023-09-03 19:33:17 +0500

    Add login function

--- ToDoListSwiftUI/ViewModels/LoginViewViewModel.swift
+++ ToDoListSwiftUI/ViewModels/LoginViewViewModel.swift
@@ -15,12 +15,17 @@ class LoginViewViewModel: ObservableObject {
     init() {}
     
     func login() {
+        errorMessage = ""
         guard !email.trimmingCharacters(in: .whitespaces).isEmpty, //trimmingCharacters = will trigger user taps a whitespace button
               !password.trimmingCharacters(in: .whitespaces).isEmpty else {
 
-            errorMessage = "Fill in all fields"
-
+            errorMessage = "Please fill in all fields."
             return // return = stop
+        }
+        //Standard writing: email@foo.com
+        guard email.contains("@") && email.contains(".") else {
+            errorMessage = "Please enter valid email."
+            return
         }
     }
     
