8d0d1f6498e4038ccb13ee3eaaeae34774a00d1c
iPakTulane <ipak.tulane@gmail.com>
2023-09-03 19:55:38 +0500

    Add login and validate functions to RegisterViewViewModel

--- ToDoListSwiftUI/ViewModels/RegisterViewViewModel.swift
+++ ToDoListSwiftUI/ViewModels/RegisterViewViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+class RegisterViewViewModel: ObservableObject {
+    @Published var name = ""
+    @Published var email = ""
+    @Published var password = ""
+    
+    init() {}
+    
+    func register() {
+        guard validate() else {
+            return
+        }
+    }
+    
+    private func validate() -> Bool {
+        guard !name.trimmingCharacters(in: .whitespaces).isEmpty,
+              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
+        
+        guard email.contains("@") && email.contains(".") else {
+            return false
+        }
+        
+        guard password.count >= 6 else {
+            return false
+        }
+        
+        return true
+    }
+    
+}
