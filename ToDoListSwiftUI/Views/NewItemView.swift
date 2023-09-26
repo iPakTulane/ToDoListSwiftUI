cc1a703f5931ecf8be7b84584f7cc432472c7e5b
iPakTulane <ipak.tulane@gmail.com>
2023-09-26 18:59:09 +0500

    Add a new item view

--- ToDoListSwiftUI/Views/NewItemView.swift
+++ ToDoListSwiftUI/Views/NewItemView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct NewItemView: View {
+    @StateObject var viewModel = NewItemViewViewModel()
+    @Binding var newItemPresented: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("New Item")
+                .font(.system(size: 32))
+                .bold()
+                .padding(.top, 100)
+            
+            Form {
+                // Title
+                TextField("Title", text: $viewModel.title)
+                    .textFieldStyle(DefaultTextFieldStyle())
+
+                // Due Date
+                DatePicker("Due Date", selection: $viewModel.dueDate)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                
+                // Button
+                TLButton(
+                    title: "Save",
+                    background: .pink
+                ) {
+                    // Action
+                    viewModel.save()
+                    newItemPresented = false
+                }
+                         .padding()
+            }
+        }
     }
 }
 
 struct NewItemView_Previews: PreviewProvider {
     static var previews: some View {
-        NewItemView()
+        NewItemView(newItemPresented: Binding(get: {
+            return true
+        }, set: { _ in
+            
+        }))
     }
 }
